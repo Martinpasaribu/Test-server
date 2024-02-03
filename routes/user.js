6111a01dce134cbc6e58aee843d06bff9fefb47d
Martinpasaribu <83689864+Martinpasaribu@users.noreply.github.com>
2024-02-03 13:06:12 +0700

    u04

--- routes/user.js
+++ routes/user.js
@@ -17,7 +17,7 @@ import multer from 'multer';
 
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
-import  {v2 as cloudinary} from 'cloudinary';
+import  cloudinary from 'cloudinary';
           
 cloudinary.config({ 
   cloud_name: 'dxkker0mf', 
