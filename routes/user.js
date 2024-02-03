d46896576e9c45c500513185e044a3e625e1cd04
Martinpasaribu <83689864+Martinpasaribu@users.noreply.github.com>
2024-02-03 14:02:22 +0700

    u07

--- routes/user.js
+++ routes/user.js
@@ -12,19 +12,21 @@ import multer from 'multer';
 // const upload = multer({ storage });
 
 
-const upload = multer({ dest: 'uploads/' })
+// const upload = multer({ dest: 'uploads/' })
 
 
-// import  {v2 as cloudinary} from 'cloudinary';
+// import { CloudinaryStorage } from 'multer-storage-cloudinary';
+
+import  {v2 as cloudinary} from 'cloudinary';
           
-// cloudinary.config({ 
-//   cloud_name: 'dxkker0mf', 
-//   api_key: '319942613824945', 
-//   api_secret: 'kiNHdZNe8x8vxuwhaitjq-cEXkM' 
-// });
-// //
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage: storage });
+cloudinary.config({ 
+  cloud_name: 'dxkker0mf', 
+  api_key: '319942613824945', 
+  api_secret: 'kiNHdZNe8x8vxuwhaitjq-cEXkM' 
+});
+//
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 
 import {getUser, getUsers , addUser, editUser, deleteUser,SaveBill,GetRiwayat }from '../controllers/user.js'
